ca765e0c6301d697347012d5e91166796aa9d8d4
ethanlaj <ethanlaj@ericlaj.com>
2018-11-25 18:53:46 -0500

    take 2

--- index.js
+++ index.js
@@ -4,20 +4,7 @@ const fs = require('fs');
 const bot = new Discord.Client({ disableEveryone: true, fetchAllMembers: true });
 bot.commands = { enabledCommands: new Discord.Collection(), disabledCommands: [] };
 bot.allcommands = new Discord.Collection();
-bot.loaders = { enabledLoaders: [], disabledLoaders: [] };
 
-var loadFile = fs.readdirSync(__dirname + '/load');
-
-for (let file of loadFile) {
-	try {
-		let loader = require('./load/' + file);
-		bot.loaders.enabledLoaders.push(loader);
-	} catch (err) {
-		bot.loaders.disabledLoaders.push(file);
-		console.log(`\nThe ${file} load module failed to load:`);
-		console.log(err);
-	}
-}
 function checkCommand(command, name) {
 	var resultOfCheck = [true, null];
 	if (!command.run) resultOfCheck[0] = false; resultOfCheck[1] = `Missing Function: "module.run" of ${name}.`;
@@ -29,7 +16,7 @@ fs.readdir('./commands/', (err, files) => {
 	if (err) console.log(err);
 	var jsfiles = files.filter((f) => f.split('.').pop() === 'js');
 	if (jsfiles.length <= 0) return console.log('Couldn\'t find commands.');
-	for (let i= 0, len = jsfiles.length; i < len; i++) {
+	for (let i = 0, len = jsfiles.length; i < len; i++) {
 		const f = jsfiles[i];
 		try {
 			var props = require(`./commands/${f}`);
@@ -46,15 +33,8 @@ fs.readdir('./commands/', (err, files) => {
 		}
 	}
 });
-bot.safeSend = function(message, name) {
-	return message.author.send(`You attempted to use the \`${name}\` command in ${message.channel.toString()}, but I cannot chat there.`);
-};
 bot.on('ready', async () => {
 	console.log(`${bot.user.tag} is online. ${bot.commands.enabledCommands.size}/${bot.commands.enabledCommands.size + bot.commands.disabledCommands.length} commands loaded successfully.`);
-	let loaders = bot.loaders.enabledLoaders;
-	for (let loader of loaders) {
-		if (loader.run != null) loader.run(bot);
-	}
 });
 bot.on('message', (message) => {
 	if (message.channel.type !== 'dm' && !message.author.bot) {
