06072b4658ad0c6b0cd84bbd730b9fb4335ba4c7
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2018-11-26 22:18:30 -0500

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -1,4 +1,4 @@
-const RESTRICTED = ['469473019425849355'];
+const RESTRICTED = ['469473019425849355', '258337390643511306'];
 const { RichEmbed } = require('discord.js');
 module.exports = {
 	run: (bot, message, args) => {
